f3f6060cb770a19a1bb43f24d6997a80adb1b2c3
oott123 <git@public.oott123.com>
2017-06-17 23:09:00 +0800

    add exception

--- index.js
+++ index.js
@@ -11,11 +11,20 @@ app.use(bodyParser.json())
 
 app.post('/ainouhook', (req, res) => {
   const payload = req.body
+  if (!payload.type || !payload.token) {
+    console.log('[AinouHook] Missing params (type or token)', payload)
+  }
   // 检查 token
-  if(payload.token !== hookToken) {
-    res.status(400).end()
+  if (payload.token !== hookToken) {
+    console.log('[AinouHook] Token mismatch, got token:', payload.token)
+    return res.status(400).end()
   }
   console.log('[AinouHook] payload:', payload)
+  if (payload.type === 'error') {
+    // 处理异步错误
+    console.log('[AinouHook] Got a error', payload.message)
+    return res.status(200).end()
+  }
   let text = readAs(payload.message, 'text').body
   let nickname = payload.user.nickname
   console.log(`[AinouHook] ${nickname} says: ${text}`)
